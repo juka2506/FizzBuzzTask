c4de2d7c4de9752d157748c812275736fe92f8b6
Julija Katukova <julija.katukova@gmail.com>
2021-07-02 18:55:42 +0300

    added alternative solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         for (int i = 1; i < 51; i++) {
             if (i % 3 == 0 && i % 7 == 0) {
                 System.out.println("FizzBuzz");
@@ -10,6 +11,22 @@ public class Main {
                 System.out.println("Fizz");
             } else if (i % 7 == 0) {
                 System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        // Alternative solution
+        for (int i = 1; i < 51; i++) {
+            if (i % 3 == 0 && i % 7 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            } else if (i % 7 == 0) {
+                System.out.println("Buzz");
+                continue;
             } else {
                 System.out.println(i);
             }
